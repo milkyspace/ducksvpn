89229aea98c6d7f1300e0a5279e48fbabcfbaeea
Владимир Колеснев <vladimirkolesnev@MacBook-Pro.local>
2024-10-23 17:23:26 +0700

    main

--- main.py
+++ main.py
@@ -1083,11 +1083,10 @@ def checkTime():
                                     e.emojize(f"Продлить подписку :money_bag:"),
                                     callback_data="PayBlock"))
                             BotChecking = TeleBot(BOTAPIKEY)
-                            print(BotChecking)
                             BotChecking.send_message(i['tgid'], texts_for_bot["alert_to_renew_sub_24hours"],
                                                      reply_markup=Butt_reffer,
                                                      parse_mode="HTML")
-
+                            print(texts_for_bot["alert_to_renew_sub_24hours"])
                             conn = pymysql.connect(host=DBHOST, user=DBUSER, password=DBPASSWORD, database=DBNAME)
                             dbCur = conn.cursor(pymysql.cursors.DictCursor)
                             dbCur.execute(f"INSERT INTO notions (tgid,notion_type) values (%s,%s)",
