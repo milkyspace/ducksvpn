588b98c3c66f5eac6715a4c11c3df5a245769542
Владимир Колеснев <vladimirkolesnev@MacBook-Pro.local>
2024-11-07 09:57:30 +0700

    main

--- main.py
+++ main.py
@@ -1120,6 +1120,8 @@ async def Buy_month(call: types.CallbackQuery):
     payment_info = await user_dat.PaymentInfo()
 
     monthСount = int(str(call.data).split(":")[1])
+    if monthСount == 100:
+        monthСount = 1
     await bot.delete_message(call.message.chat.id, call.message.id)
 
     if call.message.chat.id in CONFIG["admin_tg_id"]:
