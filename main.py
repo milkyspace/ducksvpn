ff5cec39d276217acf8a02c591a91641c662eb07
Владимир Колеснев <you@example.com>
2024-12-08 09:23:07 +0700

    main

--- main.py
+++ main.py
@@ -600,7 +600,7 @@ async def start(message: types.Message):
                 print('gift send')
                 async with bot.retrieve_data(message.chat.id) as data:
                     print(message.text)
-                    data['giftid'] = message.text.replace('gift', '')
+                    data['giftid'] = message.text.replace('/start gift', '')
                 await bot.send_message(message.chat.id, f'Для вас подготовлен подарок! :wrapped_gift:',
                                        parse_mode="HTML",
                                        reply_markup=await main_buttons(user_dat))
