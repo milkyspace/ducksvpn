5d2a230cd3312efc626542ef6b01027c943654f1
Владимир Колеснев <you@example.com>
2024-12-13 21:43:29 +0700

    main

--- main.py
+++ main.py
@@ -2210,8 +2210,8 @@ if __name__ == '__main__':
     threadcheckTime.start()
     threadcheckBackup = threading.Thread(target=checkBackup, name="checkBackup1")
     threadcheckBackup.start()
-    # threadcheckQueue = threading.Thread(target=checkQueue(), name="checkQueue1")
-    # threadcheckQueue.start()
+    threadcheckQueue = threading.Thread(target=checkQueue(), name="checkQueue1", daemon=True)
+    threadcheckQueue.start()
     # threadcheckUsers = threading.Thread(target=checkUsers(), name="checkUsers1")
     # threadcheckUsers.start()
 
